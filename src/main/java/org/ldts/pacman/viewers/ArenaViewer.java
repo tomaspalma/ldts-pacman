4b7a3b34830a33f6252286625a5819dd81114f86
DiogoMartins888 <up202108883@up.pt>
2022-11-15 10:24:43 +0000

    Create ArenaViewer class

---
+++ src/main/java/org/ldts/pacman/viewers/ArenaViewer.java
@@ -0,0 +1,15 @@
+package org.ldts.pacman.viewers;
+
+import org.ldts.pacman.gui.GUI;
+import org.ldts.pacman.models.Arena;
+
+public class ArenaViewer extends Viewer<Arena> {
+    public ArenaViewer(Arena arena) {
+        super(arena);
+    }
+
+    @Override
+    protected void drawEntities(GUI gui) {
+        //TODO
+    }
+}
