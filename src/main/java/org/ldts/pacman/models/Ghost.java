6223023ccc601ae5069288f227e5a9c6483fb25c
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-20 20:04:50 +0000

    added model attributes for color and draw symbol

--- src/main/java/org/ldts/pacman/models/Ghost.java
+++ src/main/java/org/ldts/pacman/models/Ghost.java
@@ -2,17 +2,9 @@ package org.ldts.pacman.models;
 
 public abstract class Ghost extends Entity {
     protected GhostStrategy currentStrategy;
-    //Para já isto pode ficar numa string
-    protected String color;
-    protected String drawSymbol;
 
     protected Ghost(Position position) {
         super(position);
         currentStrategy = new AliveStrategy();
     }
-
-    protected String getDrawSymbol() {
-        return drawSymbol;
-    }
-
 }
--- src/main/java/org/ldts/pacman/models/Wall.java
+++ src/main/java/org/ldts/pacman/models/Wall.java
@@ -1,7 +1,11 @@
 package org.ldts.pacman.models;
 
+import com.googlecode.lanterna.TextColor;
+
 public class Wall extends FixedEntity {
     public Wall(Position position) {
         super(position);
+        this.drawSymbol = "?";
+        this.color = TextColor.ANSI.BLUE;
     }
 }
