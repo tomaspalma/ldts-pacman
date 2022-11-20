9a5140fb9b253b68487961d7e0a758c5763dbdd4
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-20 16:12:37 +0000

    removed Float attributes and methods for int

--- src/main/java/org/ldts/pacman/models/Position.java
+++ src/main/java/org/ldts/pacman/models/Position.java
@@ -5,19 +5,19 @@ import java.util.Objects;
 public class Position {
     // Atenção a comparar os valores de uma posição com outra por causa de serem números *floating point*
     // Para os comparar utilizar um epsilon. Exemplo:
-    private final Float x;
-    private final Float y;
+    private final int x;
+    private final int y;
 
-    public Position(Float x, Float y) {
+    public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public Float getX() {
+    public int getX() {
         return x;
     }
 
-    public Float getY() {
+    public int getY() {
         return y;
     }
 
@@ -27,7 +27,7 @@ public class Position {
         if (o == null || getClass() != o.getClass()) return false;
         Position position = (Position) o;
 
-        return x.equals(position.x) && y.equals(position.y);
+        return x == position.x && y == position.y;
     }
 
     // Como para objetos iguais o hashcode tem de ser o mesmo e duas posições são iguais se o x e o y forem iguais
