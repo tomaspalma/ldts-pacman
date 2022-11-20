deee712a4d9f6b5227d3b031d375705156acdf69
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-20 19:26:39 +0000

    Added draw symbol to model of ghosts

--- src/main/java/org/ldts/pacman/models/Ghost.java
+++ src/main/java/org/ldts/pacman/models/Ghost.java
@@ -1,14 +1,18 @@
 package org.ldts.pacman.models;
 
 public abstract class Ghost extends Entity {
-    enum GHOST_STATE {}
     protected GhostStrategy currentStrategy;
     //Para já isto pode ficar numa string
     protected String color;
+    protected String drawSymbol;
 
     protected Ghost(Position position) {
         super(position);
         currentStrategy = new AliveStrategy();
+    }
+
+    protected String getDrawSymbol() {
+        return drawSymbol;
     }
 
 }
--- src/main/java/org/ldts/pacman/models/RegularGhost.java
+++ src/main/java/org/ldts/pacman/models/RegularGhost.java
@@ -3,5 +3,6 @@ package org.ldts.pacman.models;
 public class RegularGhost extends Ghost {
     public RegularGhost(Position position) {
         super(position);
+        this.drawSymbol = "E";
     }
 }
