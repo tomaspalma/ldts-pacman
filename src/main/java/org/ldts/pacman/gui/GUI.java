41b8bad2dc98f111b1e7a1c919940371f0fa389d
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-20 19:22:22 +0000

    Create a terminal and a screen

--- src/main/java/org/ldts/pacman/gui/GUI.java
+++ src/main/java/org/ldts/pacman/gui/GUI.java
@@ -4,10 +4,18 @@ import java.io.IOException;
 
 // Abstração entre o resto dos elementos do programa e possíveis librarias de GUI que quisermos utilizar
 // Contém métodos gerais abstraídos que vamos querer que uma determinada GUI implemente
+// Aqui só estarão os métodos que pretendemos que possam ser executados de forma pública em outras partes do código
 public interface GUI {
+    // Funções para o screen
     void clear();
 
     void refresh() throws IOException;
 
     void close() throws IOException;
+
+    void gracefulExit() throws IOException;
+
+    void hideCursor();
+    void showCursor();
+
 }
--- src/main/java/org/ldts/pacman/gui/GUIForLanterna.java
+++ src/main/java/org/ldts/pacman/gui/GUIForLanterna.java
@@ -1,14 +1,46 @@
 package org.ldts.pacman.gui;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class GUIForLanterna implements GUI {
     private final Screen screen;
+    private final Terminal terminal;
+    private final TextGraphics graphics;
 
+    /*
     public GUIForLanterna(Screen screen) {
         this.screen = screen;
+    }*/
+
+    // O código de criar terminal só deve ser corrido uma vez por objeto de GUI
+    public GUIForLanterna(int width, int height) throws IOException {
+        this.terminal = this.createTerminalScreen(width, height);
+        this.terminal.enterPrivateMode();
+
+        this.screen = createScreen(terminal);
+        this.graphics = this.screen.newTextGraphics();
+    }
+
+    public TerminalSize getTerminalSize() throws IOException {
+        return terminal.getTerminalSize();
+    }
+
+    public Terminal getTerminal() {
+        return terminal;
+    }
+
+    public Screen getScreen() {
+        return screen;
     }
 
     @Override
@@ -23,6 +55,34 @@ public class GUIForLanterna implements GUI {
 
     @Override
     public void close() throws IOException {
-        screen.close();
+        this.screen.close();
     }
+
+    @Override
+    public void gracefulExit() throws IOException {
+        if(this.terminal != null) this.terminal.exitPrivateMode();
+    }
+
+    @Override
+    public void hideCursor() {
+
+    }
+
+    @Override
+    public void showCursor() {
+
+    }
+
+    private Terminal createTerminalScreen(int width, int height) throws IOException {
+        TerminalSize terminalSize = new TerminalSize(width, height);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        return terminalFactory.createTerminal();
+    }
+
+    private Screen createScreen(Terminal terminal) throws IOException {
+        Screen resultScreen = new TerminalScreen(terminal);
+        resultScreen.startScreen();
+        return resultScreen;
+    }
+
 }
