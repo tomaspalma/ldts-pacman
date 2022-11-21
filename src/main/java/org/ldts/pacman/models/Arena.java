2b12549e7cda140fbcb5fa42633dc31b44edd901
DiogoMartins888 <up202108883@up.pt>
2022-11-21 22:32:35 +0000

    Add placeholder walls and ghosts

--- src/main/java/org/ldts/pacman/models/Arena.java
+++ src/main/java/org/ldts/pacman/models/Arena.java
@@ -1,8 +1,14 @@
 package org.ldts.pacman.models;
 
+import java.util.List;
+
 public class Arena {
     private final int height;
     private final int width;
+    private Pacman pacman;
+
+    private List<Wall> walls;
+    private List<Ghost> ghosts;
 
     public Arena(int width, int height){
         this.width = width;
@@ -15,5 +21,31 @@ public class Arena {
 
     public int getWidth() {
         return width;
+    }
+
+    public Pacman getPacman() {
+        return pacman;
+    }
+
+    public void setPacman(Pacman pacman) {
+        this.pacman = pacman;
+    }
+
+    public void setWalls(List<Wall> walls) {
+        this.walls = walls;
+    }
+
+    public void setGhosts(List<Ghost> ghosts) {
+        this.ghosts = ghosts;
+    }
+
+    // TODO
+    public boolean isWall(Position position) {
+        return false;
+    }
+
+    // TODO
+    public boolean isGhost(Position position) {
+        return false;
     }
 }
