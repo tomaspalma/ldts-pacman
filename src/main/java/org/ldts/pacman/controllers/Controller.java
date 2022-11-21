9323662582ff8390331c439e7d35c72952f317a1
DiogoMartins888 <up202108883@up.pt>
2022-11-21 11:34:00 +0000

    Add abstract step method

--- src/main/java/org/ldts/pacman/controllers/Controller.java
+++ src/main/java/org/ldts/pacman/controllers/Controller.java
@@ -1,5 +1,10 @@
 package org.ldts.pacman.controllers;
 
+import org.ldts.pacman.App;
+import org.ldts.pacman.gui.GUI;
+
+import java.io.IOException;
+
 // O controller vai processar e efetuar mudanças num modelo
 public abstract class Controller<T> {
     private final T model;
@@ -11,4 +16,6 @@ public abstract class Controller<T> {
     public T getModel() {
         return model;
     }
+
+    public abstract void step(App game, GUI.ACTION action, long time) throws IOException;
 }
