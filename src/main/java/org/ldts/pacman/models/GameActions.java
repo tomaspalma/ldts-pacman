b3cf3882799e27bcc979ffb305e22848962bc882
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-21 11:52:30 +0000

    Added new enum for control actions the user can issue to the game

--- src/main/java/org/ldts/pacman/models/GameActions.java
+++ src/main/java/org/ldts/pacman/models/GameActions.java
@@ -1,8 +1,17 @@
 package org.ldts.pacman.models;
 
 public class GameActions {
-    public enum ghostCollisionWithPacman {
+    public enum GhostCollisionWithPacman {
        KILL_PACMAN,
        KILL_GHOST,
+    }
+
+    public enum ControlActions {
+        MOVE_LEFT,
+        MOVE_RIGHT,
+        MOVE_UP,
+        MOVE_DOWN,
+        EXIT,
+        NONE,
     }
 }
