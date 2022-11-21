6cc5ba0b815ce57c4f4da13041493dd9faa5764b
DiogoMartins888 <up202108883@up.pt>
2022-11-21 22:04:43 +0000

    Add methods to get adjacent positions

--- src/main/java/org/ldts/pacman/models/Position.java
+++ src/main/java/org/ldts/pacman/models/Position.java
@@ -21,6 +21,21 @@ public class Position {
         return y;
     }
 
+    public Position getLeft() {
+        return new Position(x - 1, y);
+    }
+
+    public Position getRight() {
+        return  new Position(x + 1, y);
+    }
+
+    public Position getUp() {
+        return new Position(x, y - 1);
+    }
+    public Position getDown() {
+        return new Position(x, y + 1);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
