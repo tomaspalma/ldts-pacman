fee92c0940dd868813ba8da46fbab127d279cfe2
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-22 21:31:26 +0000

    Testing for relative methods from position

---
+++ src/test/groovy/org/ldts/pacman/PositionTest.groovy
@@ -0,0 +1,20 @@
+package org.ldts.pacman
+
+import org.ldts.pacman.models.Position
+import spock.lang.Specification
+
+class PositionTest extends Specification {
+    private def pos;
+
+    def setup() {
+        pos = new Position(15, 15);
+    }
+
+    def "We should be able to get relative positions from one"() {
+        expect:
+            pos.getPositionToTheLeft() == new Position(14, 15)
+            pos.getPositionToTheRight() == new Position(16, 15)
+            pos.getPositionAbove() == new Position(15, 14)
+            pos.getPositionBelow() == new Position(15, 16)
+    }
+}
