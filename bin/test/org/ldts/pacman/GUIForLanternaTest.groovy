a12ac33225490a36c182d4872c9143a0a58fafc8
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-22 16:53:59 +0000

    added more tests

--- bin/test/org/ldts/pacman/GUIForLanternaTest.groovy
+++ bin/test/org/ldts/pacman/GUIForLanternaTest.groovy
@@ -1,9 +1,8 @@
 import com.googlecode.lanterna.TerminalSize
-import com.googlecode.lanterna.TextColor
 import com.googlecode.lanterna.bundle.LanternaThemes
 import org.ldts.pacman.gui.GUI
 import org.ldts.pacman.gui.GUIForLanterna
-import org.ldts.pacman.models.Position
+
 import spock.lang.Specification
 
 class GUIForLanternaTest extends Specification {
@@ -30,21 +29,6 @@ class GUIForLanternaTest extends Specification {
     def "We should be able to get the correct lanterna terminal size"() {
         expect:
             lanternaGUI.getTerminalSize() == new TerminalSize(200, 200)
-    }
-
-    def "We should execute the specific draw for our element"() {
-        given:
-            Position position = GroovyMock(Position)
-            TextColor.ANSI textColor = GroovyMock(TextColor.ANSI)
-            String drawSymbol = GroovyMock(String)
-        when:
-            lanternaGUI.drawEntity(position, textColor, drawSymbol)
-        then:
-            1 * lanternaGUI.drawElement(position, textColor, drawSymbol)
-    }
-
-    def "Pressing a keystroke should yield respective response"() {
-
     }
 
 }
