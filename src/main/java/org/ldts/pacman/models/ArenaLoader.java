bb3300ce48738c519bd5ec7d681e89d4b9f6055b
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-23 21:36:29 +0000

    Added basic structure for class to load arena from map

---
+++ src/main/java/org/ldts/pacman/models/ArenaLoader.java
@@ -0,0 +1,17 @@
+package org.ldts.pacman.models;
+
+import java.util.List;
+
+public class ArenaLoader {
+    private final String mapToLoad;
+
+    public ArenaLoader(String mapToLoad) {
+       this.mapToLoad = mapToLoad;
+    }
+
+    // Ler o mapa de um fichieor e carregar as entidades corretas para a lista
+    public void load(List<Entity> entitiesToLoad) {
+
+    }
+
+}
