f8a8fa715391a513318fe79df5fb28eebcc126f3
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-24 08:50:10 +0000

    fixed issues with last weird commit

--- src/main/java/org/ldts/pacman/models/Pacman.java
+++ src/main/java/org/ldts/pacman/models/Pacman.java
@@ -1,17 +1,4 @@
 package org.ldts.pacman.models;
-
-<<<<<<< HEAD
-import java.util.HashMap;
-
-public class Pacman extends MovableEntity {
-    private HashMap<String, Integer> orientationIndexCorrespondence;
-    private static final String[] drawSymbols = {"A", "B", "C", "D"};
-
-    public Pacman(Position position) {
-        super(position);
-        // Aqui depois temos que ver qual é a orientação padrão que o pacman irá ter (se é que existirá uma)
-        this.drawSymbol = drawSymbols[0];
-=======
 import com.googlecode.lanterna.TextColor;
 
 import java.util.Arrays;
@@ -20,13 +7,12 @@ import java.util.HashMap;
 
 public class Pacman extends MovableEntity {
     private final HashMap<String, Integer> orientationIndexCorrespondence = new HashMap<>();
-    private List<String> drawSymbols = Arrays.asList("A", "B", "C", "D");
+    private String[] drawSymbols = {"A", "B", "C", "D"};
     public Pacman(Position position) {
         super(position);
         // Aqui depois temos que ver qual é a orientação padrão que o pacman irá ter (se é que existirá uma)
-        this.drawSymbol = drawSymbols.get(0);
+        this.drawSymbol = drawSymbols[0];
         this.color = TextColor.ANSI.YELLOW;
->>>>>>> 456124498d166da454a8e1cf6c28def2ee831847
         orientationIndexCorrespondence.put("UP", 0);
         orientationIndexCorrespondence.put("DOWN", 1);
         orientationIndexCorrespondence.put("LEFT", 2);
