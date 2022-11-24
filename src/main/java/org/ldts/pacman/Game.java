38b6e70edd2e8d421e2e86583ec9109104e5af24
DiogoMartins888 <up202108883@up.pt>
2022-11-24 22:47:17 +0000

    Change from 60 to 10 fps

--- src/main/java/org/ldts/pacman/Game.java
+++ src/main/java/org/ldts/pacman/Game.java
@@ -28,7 +28,7 @@ public class Game {
     }
 
     private void run() throws IOException, InterruptedException {
-        int FPSLimit = 60;
+        int FPSLimit = 10;
         int frameTime = 1000 / FPSLimit;
 
         while(this.currentState != null) {
