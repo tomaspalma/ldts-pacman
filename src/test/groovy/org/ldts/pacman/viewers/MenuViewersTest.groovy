493050686cfc7bf5ba6116b5ec54fb529395aece
DiogoMartins888 <up202108883@up.pt>
2022-12-18 15:55:11 +0000

    Test RegularMenuViewer

--- src/test/groovy/org/ldts/pacman/viewers/MenuViewersTest.groovy
+++ src/test/groovy/org/ldts/pacman/viewers/MenuViewersTest.groovy
@@ -1,6 +1,30 @@
 package org.ldts.pacman.viewers
 
+import com.googlecode.lanterna.TextColor
+import org.ldts.pacman.models.game.Position
+import org.ldts.pacman.viewers.menus.RegularMenuViewer
 import spock.lang.Specification
+import org.ldts.pacman.models.menus.Menu
+import org.ldts.pacman.gui.GUI
 
 class MenuViewersTest extends Specification {
+    private def gui
+
+    def setup() {
+        gui = Mock(GUI.class)
+    }
+
+    def "regular viewer"() {
+        given:
+        def menu = Mock(Menu.class)
+        def menuViewer = new RegularMenuViewer(menu)
+
+        when:
+        menuViewer.drawEntities(gui)
+
+        then:
+        1 * gui.writeToScreen(_ as Position, menu.getTitle(), menu.getColor())
+        menu.getNumberOptions() * menu.getOption(_)
+        menu.getNumberOptions() * gui.writeToScreen(_ as Position, _ as String, _ as TextColor.ANSI)
+    }
 }
