91db0a8b378fd4f47a1c296a99929cdf9960c42f
DiogoMartins888 <up202108883@up.pt>
2022-12-18 15:58:09 +0000

    Test PauseMenuViewer

--- src/test/groovy/org/ldts/pacman/viewers/MenuViewersTest.groovy
+++ src/test/groovy/org/ldts/pacman/viewers/MenuViewersTest.groovy
@@ -2,10 +2,12 @@ package org.ldts.pacman.viewers
 
 import com.googlecode.lanterna.TextColor
 import org.ldts.pacman.models.game.Position
+import org.ldts.pacman.viewers.menus.PauseMenuViewer
 import org.ldts.pacman.viewers.menus.RegularMenuViewer
 import spock.lang.Specification
 import org.ldts.pacman.models.menus.Menu
 import org.ldts.pacman.gui.GUI
+import org.ldts.pacman.models.menus.PauseMenu
 
 class MenuViewersTest extends Specification {
     private def gui
@@ -18,6 +20,20 @@ class MenuViewersTest extends Specification {
         given:
         def menu = Mock(Menu.class)
         def menuViewer = new RegularMenuViewer(menu)
+
+        when:
+        menuViewer.drawEntities(gui)
+
+        then:
+        1 * gui.writeToScreen(_ as Position, menu.getTitle(), menu.getColor())
+        menu.getNumberOptions() * menu.getOption(_)
+        menu.getNumberOptions() * gui.writeToScreen(_ as Position, _ as String, _ as TextColor.ANSI)
+    }
+
+    def "pause viewer"() {
+        given:
+        def menu = Mock(PauseMenu.class)
+        def menuViewer = new PauseMenuViewer(menu)
 
         when:
         menuViewer.drawEntities(gui)
